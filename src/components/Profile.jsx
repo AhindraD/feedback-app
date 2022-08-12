0134f4670d648ffd2be7e6e31365cb16511ec399
Ahindra D <official.ahindra@gmail.com>
2022-08-12 12:15:57 +0530

    displaying

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -10,7 +10,7 @@ import { database, auth } from "../firebase-config";
 
 export default function Profile() {
     let { user, userID } = useContext(UserContext);
-    let [usersData, setUsersData] = useState({});
+    let [userData, setUserData] = useState({});
     let [loaded, setLoaded] = useState(false);
 
     let goTo = useNavigate();
@@ -21,26 +21,41 @@ export default function Profile() {
             alert('Please Log in!');
             goTo('/');
         }
-        console.log(userID);
-        const roomsRef = ref(database, 'users/'+userID);
+        //console.log(userID);
+        const roomsRef = ref(database, 'users/' + userID);
         onValue(roomsRef, (snapshot) => {
             let data = snapshot.val();
             //console.log(data);
             data = data === undefined ? {} : data;
-            console.log(data);
-            setUsersData(data);
+            //console.log(data);
+            setUserData(data);
             setLoaded(true);
         });
     }, [])
 
     return (
         <div className="profile-cont">
-            <p className="profile-name">{ }</p>
+            <p className="profile-name">{userData.userName}</p>
             <img src="./images/avatar1.png" alt="" />
             {/* <Avatar /> */}
             <section className="feedback-cont">
                 <p className='title-f'>FeedBacks:</p>
+                {loaded ?
+                    userData.feedbacks.map((elem, indx) => {
+                        return <div className="f-card" key={indx}>
+                            <p className='f-text'>{elem}</p>
+                            <button className="f-del">✖</button>
+                        </div>
+                    })
+                    : null}
             </section>
         </div>
     )
 }
+
+/*
+feedbacks: ['test-feedback']
+userEmail: "ahindra2@mail.com"
+userId: "ahindra2@mail_dot_com"
+userName: "ahin2"
+*/
