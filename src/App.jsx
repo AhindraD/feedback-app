fff065b9147a5d2948766a9873a7bf0096badda7
Ahindra D <official.ahindra@gmail.com>
2022-08-12 09:32:49 +0530

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css';
-import UserContext from './Contexts/UserContext'
-
+import UserContext from './Contexts/UserContext';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Profile from './components/Profile';
+import FeedBack from './components/FeedBack';
 function App() {
   return (
     <UserContext.Provider value={{}}>
       <div className="App">
-
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/:feedBackID' element={<FeedBack />} />
+          {/* <Route path='*' element={<NoMatch />} /> */}
+        </Routes>
       </div>
     </UserContext.Provider>
   );
---
+++ src/components/FeedBack.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+// import { useContext } from "react";
+// import ProductContext from "../contexts/ProductContext";
+//let { cart, setCart, value, setValue, cartPos, setCartPos } = useContext(ProductContext);
+export default function FeedBack() {
+    return (
+        <div>FeedBack</div>
+    )
+}
